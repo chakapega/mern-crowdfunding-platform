5fef2d27ae203b25d80c8f4dd90e37e6735fc3eb
chakapega <chakapega@gmail.com>
2020-01-30 05:19:49 +0300

    feat: implemented authorization logic

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const express = require("express");
-const config = require("config");
-const mongoose = require("mongoose");
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
 
 const app = express();
-const PORT = config.get("port");
+const PORT = config.get('port');
+
+app.use('/api/auth', require('./routes/auth.routes'));
 
 async function start() {
   try {
-    await mongoose.connect(config.get("mongoUri"), {
+    await mongoose.connect(config.get('mongoUri'), {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
     });
-    app.listen(PORT, () => console.log("App started"));
+    app.listen(PORT, () => console.log('App started'));
   } catch (error) {
-    console.log("Server error", error.message);
+    console.log('Server error', error.message);
     process.exit(1);
   }
 }
