84b23fa844874cb505b6a9dcaf905aa7e8267ca6
chakapega <chakapega@gmail.com>
2020-02-04 04:52:53 +0300

    feat: Validation of the new project creation form is made, except for the image upload component

--- client/src/components/projects/CreateProjectPage.js
+++ client/src/components/projects/CreateProjectPage.js
@@ -10,21 +10,23 @@ export default class CreateProjectPage extends Component {
     const year = currentDate.getFullYear();
     let month = currentDate.getMonth() + 1;
     let date = currentDate.getDate();
-    if (date < 10) date += '0';
-    if (month < 10) month += '0';
+    /* eslint-disable */
+    if (date < 10) date = '0' + date;
+    if (month < 10) month = '0' + month;
+    /* eslint-enable */
     const minimumDate = `${year}-${month}-${date}`;
     this.state = {
       name: '',
       description: '',
-      category: 'Technology',
+      category: '',
       tags: '',
-      images: [],
-      video: '',
       fundraisingEndDate: '',
       target: '',
       bonusTen: '',
       bonusTwentyFive: '',
       bonusFifty: '',
+      video: '',
+      images: [],
       minimumDate
     };
   }
@@ -57,15 +59,16 @@ export default class CreateProjectPage extends Component {
       <Form className='container mt-3' id='create-project-form' onSubmit={this.handleSubmit}>
         <Form.Group>
           <Form.Label>Project name</Form.Label>
-          <Form.Control name='name' type='text' placeholder='Project name' onChange={this.handleInputChange} />
+          <Form.Control name='name' type='text' placeholder='Project name' required onChange={this.handleInputChange} />
         </Form.Group>
         <Form.Group>
           <Form.Label>Project description</Form.Label>
-          <Form.Control name='description' as='textarea' rows='3' onChange={this.handleInputChange} />
+          <Form.Control name='description' as='textarea' rows='3' required onChange={this.handleInputChange} />
         </Form.Group>
         <Form.Group>
           <Form.Label>Category</Form.Label>
-          <Form.Control name='category' as='select' onChange={this.handleInputChange}>
+          <Form.Control name='category' as='select' required onChange={this.handleInputChange}>
+            <option>...</option>
             <option>Technology</option>
             <option>Education</option>
             <option>Food</option>
@@ -75,7 +78,7 @@ export default class CreateProjectPage extends Component {
         </Form.Group>
         <Form.Group>
           <Form.Label>Tags</Form.Label>
-          <Form.Control name='tags' type='text' placeholder='Tags...' onChange={this.handleInputChange} />
+          <Form.Control name='tags' type='text' placeholder='Tags...' required onChange={this.handleInputChange} />
           <Form.Text className='text-muted'>Enter tags separated by commas</Form.Text>
         </Form.Group>
         <Form.Label>Target amount of money</Form.Label>
@@ -83,36 +86,48 @@ export default class CreateProjectPage extends Component {
           <InputGroup.Prepend>
             <InputGroup.Text>$</InputGroup.Text>
           </InputGroup.Prepend>
-          <Form.Control name='target' type='number' onChange={this.handleInputChange} />
+          <Form.Control name='target' type='number' required onChange={this.handleInputChange} />
         </InputGroup>
         <Form.Group>
           <Form.Label>Fundraising End Date (mm/dd/yyyy)</Form.Label>
-          <Form.Control name='fundraisingEndDate' type='date' min={minimumDate} onChange={this.handleInputChange} />
+          <Form.Control
+            name='fundraisingEndDate'
+            type='date'
+            min={minimumDate}
+            required
+            onChange={this.handleInputChange}
+          />
         </Form.Group>
         <InputGroup className='mb-3'>
           <InputGroup.Prepend>
             <InputGroup.Text>$ 10 Bonus</InputGroup.Text>
           </InputGroup.Prepend>
-          <FormControl name='bonusTen' as='textarea' onChange={this.handleInputChange} />
+          <FormControl name='bonusTen' as='textarea' required onChange={this.handleInputChange} />
         </InputGroup>
         <InputGroup className='mb-3'>
           <InputGroup.Prepend>
             <InputGroup.Text>$ 25 Bonus</InputGroup.Text>
           </InputGroup.Prepend>
-          <FormControl name='bonusTwentyFive' as='textarea' onChange={this.handleInputChange} />
+          <FormControl name='bonusTwentyFive' as='textarea' required onChange={this.handleInputChange} />
         </InputGroup>
         <InputGroup className='mb-3'>
           <InputGroup.Prepend>
             <InputGroup.Text>$ 50 Bonus</InputGroup.Text>
           </InputGroup.Prepend>
-          <FormControl name='bonusFifty' as='textarea' onChange={this.handleInputChange} />
+          <FormControl name='bonusFifty' as='textarea' required onChange={this.handleInputChange} />
         </InputGroup>
         <Form.Group>
-          <ImageUploader onChange={this.onDrop} withPreview />
+          <ImageUploader withPreview onChange={this.onDrop} />
         </Form.Group>
         <Form.Group>
           <Form.Label>Link to YouTube video</Form.Label>
-          <Form.Control name='video' type='url' placeholder='Link to YouTube video' onChange={this.handleInputChange} />
+          <Form.Control
+            name='video'
+            type='url'
+            placeholder='Link to YouTube video'
+            required
+            onChange={this.handleInputChange}
+          />
           <Form.Text className='text-muted'>Example: https://www.youtube.com/...</Form.Text>
         </Form.Group>
         <Button className='mb-5' type='submit'>
