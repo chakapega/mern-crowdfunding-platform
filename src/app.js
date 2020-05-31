949e7115dec8ae2f70635b06f5ef5115534312ec
chakapega <chakapega@gmail.com>
2020-06-01 02:18:11 +0300

    feat: moved part of the code to run the application on the server

--- src/app.js
+++ src/app.js
@@ -9,13 +9,6 @@ const projectRouter = require('./resources/projects/project.router');
 const searchRouter = require('./resources/search/search.router');
 const userRouter = require('./resources/users/user.router');
 
-if (NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 app.use(express.json());
 
 app.use('/api', authorizationRouter);
@@ -25,5 +18,12 @@ app.use('/api', searchRouter);
 app.use('/api', userRouter);
 
 app.use(errorHandler);
+
+if (NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 module.exports = app;
